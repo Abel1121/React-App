09de5484dacb110bcbd24e4a2fe83f6bd0fbc4db
Marek Kobyliński <Marecki1121@gmail.com>
2019-10-15 23:19:47 +0200

    add: components/assests/data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import ListWrapper from './components/ListWrapper/ListWrapper';
+import './index.css';
 
 const App = () => (
     <div className="wrapper">
         <h1 className="mainHeader">Hello World</h1>
         <h2 className="secondaryHeader">Hello City</h2>
+        <ListWrapper />
     </div>
+    
 );
 export default App;
