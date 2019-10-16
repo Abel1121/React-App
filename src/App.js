e9115a3499b454a6b29fb648f74a09cdb9032e9b
Marek Kobyliński <Marecki1121@gmail.com>
2019-10-16 20:20:15 +0200

    feat: style for listItem & listWrapper

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import './index.css';
 
 const App = () => (
     <div className="wrapper">
-        <h1 className="mainHeader">Hello World</h1>
-        <h2 className="secondaryHeader">Hello City</h2>
         <ListWrapper />
     </div>
     
