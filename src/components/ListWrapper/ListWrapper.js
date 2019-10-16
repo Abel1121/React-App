9ac9e2cea9bfb388c9f17dd369763359a15678d7
Marek Kobyliński <Marecki1121@gmail.com>
2019-10-16 21:14:00 +0200

    add: props & PropTypes

--- src/components/ListWrapper/ListWrapper.js
+++ src/components/ListWrapper/ListWrapper.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ListItem from './ListItem/ListItem';
 import './ListWrapper.css';
+import {twitterAccounts} from '../../data/twitterAccounts';
 
 const ListWrapper = () => (
     <ul className="listWrapper__wrapper">
-        <ListItem />
-        <ListItem />
-        <ListItem />
-        <ListItem />
+        {twitterAccounts.map(item => (
+            <ListItem key={item.name}{...item} />
+        ))}
     </ul>
 );
 
