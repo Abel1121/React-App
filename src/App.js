2c13194cc23e9a0c69e8993283b82c6dd460cafb
Marek Kobyliński <Marecki1121@gmail.com>
2019-10-18 16:26:26 +0200

    feat: change const App to class App extendets

--- src/App.js
+++ src/App.js
@@ -2,10 +2,47 @@ import React from 'react';
 import ListWrapper from './components/ListWrapper/ListWrapper';
 import './index.css';
 
-const App = () => (
-    <div className="wrapper">
-        <ListWrapper />
-    </div>
+const initialStateOperator = [
+        {
+        image: 'https://pbs.twimg.com/profile_images/1166344766210150401/amRnWzl-_400x400.jpg',
+        name: 'Dan Abramov',
+        description: 'Working on @reactjs. The demo guy.',
+        twitterLink: 'https://twitter.com/dan_abramov',
+    },
+    {
+        image: 'https://pbs.twimg.com/profile_images/1166030195834273794/pBb6hjVb_400x400.jpg',
+        name: 'Ryan Florence',
+        description: 'Making React accessible for users and developers at https://reach.tech . Online learning, workshops, OSS, and consulting.',
+        twitterLink: 'https://twitter.com/ryanflorence',
+    },
+    {
+        image: 'https://avatars0.githubusercontent.com/u/92839?s=460&v=4',
+        name: 'Michael Jackson',
+        description: 'Maker. Co-author of React Router. Working on @ReactTraining. Created @unpkg. Head over heels for @cari.',
+        twitterLink: 'https://twitter.com/mjackson',
+    },
+    {
+        image: 'https://avatars0.githubusercontent.com/u/1500684?v=4',
+        name: 'Kent C. Dodds',
+        description: 'Making software development more accessible · Husband, Father, Latter-day Saint, Teacher, OSS, GDE, @TC39 · @PayPalEng @eggheadio @FrontendMasters · #JS',
+        twitterLink: 'https://twitter.com/kentcdodds',
+    }, 
+]
+
+class App extends React.Component {
+    state = {
+        items: [...initialStateOperator],
+    }
+    render() {
+        return (
+            <div>
+                <ListWrapper 
+                    items={this.state.items}
+                />
+            </div>
+
+        )
+    }
     
-);
+};
 export default App;
